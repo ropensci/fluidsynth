1efcf6c2e0ccdb9be7f8b30b2eaf79ba5225a33a
Jeroen Ooms <jeroenooms@gmail.com>
2024-02-12 12:53:51 +0100

    add skeleton

---
+++ src/init.c
@@ -0,0 +1,16 @@
+#include <Rinternals.h>
+#include <stdlib.h>
+#include <R_ext/Rdynload.h>
+
+/* .Call calls */
+extern SEXP C_midi_play(SEXP, SEXP, SEXP);
+
+static const R_CallMethodDef CallEntries[] = {
+  {"C_midi_play", (DL_FUNC) &C_midi_play, 3},
+  {NULL, NULL, 0}
+};
+
+void R_init_miditools(DllInfo *dll){
+  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+}
