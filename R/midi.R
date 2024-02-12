67b8d4542ce582688013c4809c719ecd6b120f27
Jeroen Ooms <jeroenooms@gmail.com>
2024-02-12 21:20:01 +0100

    Also enable progress for playing

--- R/midi.R
+++ R/midi.R
@@ -9,10 +9,12 @@
 #' @rdname miditools
 #' @param midi path to the midi file
 #' @param soundfont path to the soundfont
-midi_play <- function(midi = demo_midi(), soundfont = general_user_gs()){
+midi_play <- function(midi = demo_midi(), soundfont = general_user_gs(), progress = TRUE){
   midi <- normalizePath(midi, mustWork = TRUE)
   soundfont <- normalizePath(soundfont, mustWork = TRUE)
-  .Call(C_midi_play, midi, soundfont, NULL, FALSE)
+  progress <- as.logical(progress)
+  .Call(C_midi_play, midi, soundfont, NULL, progress)
+  invisible()
 }
 
 #' @rdname miditools
@@ -26,6 +28,7 @@ midi_convert <- function(midi = demo_midi(), soundfont = general_user_gs(), outp
   output <- normalizePath(output, mustWork = FALSE)
   progress <- as.logical(progress)
   .Call(C_midi_play, midi, soundfont, output, progress)
+  output
 }
 
 general_user_gs <- function(){
--- src/playmidi.c
+++ src/playmidi.c
@@ -51,21 +51,21 @@ SEXP C_midi_play(SEXP midi, SEXP soundfont, SEXP outfile, SEXP progress){
     if(output_file){
       if(fluid_file_renderer_process_block(renderer) != FLUID_OK)
         break;
-      if(Rf_asLogical(progress)){
-        int now = fluid_player_get_current_tick(player);
-        if(total == 0)
-          total = fluid_player_get_total_ticks(player);
-        if(now < total){
-          REprintf("\r%d/%d", now, total);
-        } else {
-          REprintf("\r");
-        }
-      }
     } else {
       usleep(200);
     }
     if(pending_interrupt()){
       fluid_player_stop(player);
+    }
+    if(Rf_asLogical(progress)){
+      int now = fluid_player_get_current_tick(player);
+      if(total == 0)
+        total = fluid_player_get_total_ticks(player);
+      if(now < total){
+        REprintf("\r%d/%d", now, total);
+      } else {
+        REprintf("\r");
+      }
     }
   }
 
