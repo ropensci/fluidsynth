49ed7b0677a7ed206ce42bf392bba68aa3ef38fe
Jeroen Ooms <jeroenooms@gmail.com>
2024-02-13 18:18:54 +0100

    Add Windows

---
+++ tools/winlibs.R
@@ -0,0 +1,18 @@
+if(!file.exists("../windows/fluidsynth/include/fluidsynth.h")){
+  unlink("../windows", recursive = TRUE)
+  url <- if(grepl("aarch", R.version$platform)){
+    "https://github.com/r-windows/bundles/releases/download/fluidsynth-2.3.4/fluidsynth-2.3.4-clang-aarch64.tar.xz"
+  } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
+    "https://github.com/r-windows/bundles/releases/download/fluidsynth-2.3.4/fluidsynth-2.3.4-clang-x86_64.tar.xz"
+  } else if(getRversion() >= "4.2") {
+    "https://github.com/r-windows/bundles/releases/download/fluidsynth-2.3.4/fluidsynth-2.3.4-ucrt-x86_64.tar.xz"
+  } else {
+    stop("This package requires at least R-4.2 on Windows")
+  }
+  download.file(url, basename(url), quiet = TRUE)
+  dir.create("../windows", showWarnings = FALSE)
+  untar(basename(url), exdir = "../windows", tar = 'internal')
+  unlink(basename(url))
+  setwd("../windows")
+  file.rename(list.files(), 'fluidsynth')
+}
