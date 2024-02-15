3de6dc6d886302808f9fda6d3b518c22f7c8a5f4
Jeroen Ooms <jeroenooms@gmail.com>
2024-02-15 15:25:09 +0100

    More soundfont stuff

--- R/soundfonts.R
+++ R/soundfonts.R
@@ -5,17 +5,16 @@
 #' midi sounds very poor, try seleting another soundfont.
 #'
 #' [GenralUser-GS](https://schristiancollins.com/generaluser) by S. Christian Collins
-#' is a nice free soundfont. The `soundfont_generaluser_gs()` function will
-#' automatically download a copy to for this package.
+#' is a nice free soundfont. The `download_generaluser_gs()` function will
+#' automatically download a copy to that will be picked up by this package.
 #'
 #' @export
 #' @name soundfonts
 #' @rdname soundfonts
 #' @param download automatically download soundfont if none exists.
 soundfont_path <- function(download = TRUE){
-  general_user <- soundfont_generaluser_gs(download = FALSE)
-  if(file.exists(general_user)){
-    return(general_user)
+  if(file.exists(generaluser_gs_path())){
+    return(generaluser_gs_path())
   }
   default <- fluidsynth_setting_default('synth.default-soundfont')
   if(file.exists(default)){
@@ -26,9 +25,9 @@ soundfont_path <- function(download = TRUE){
 
 #' @export
 #' @rdname soundfonts
-soundfont_generaluser_gs <- function(download = TRUE){
-  path <- file.path(system.file(package = 'fluidsynth'), 'generaluser-gs/v1.471.sf2')
-  if(!file.exists(path) && isTRUE(download)){
+download_generaluser_gs <- function(){
+  path <- generaluser_gs_path()
+  if(!file.exists(path)){
     url <- 'https://github.com/ropensci/fluidsynth/releases/download/generaluser-gs-v1.471/generaluser-gs-v1.471.zip'
     if(getOption('timeout') < 300) options(timeout = 300)
     on.exit(unlink('generaluser-gs-v1.471.zip'))
@@ -37,4 +36,9 @@ soundfont_generaluser_gs <- function(download = TRUE){
     unzip('generaluser-gs-v1.471.zip', exdir = dirname(path))
   }
   return(path)
+}
+
+
+generaluser_gs_path <- function(){
+  file.path(system.file(package = 'fluidsynth'), 'generaluser-gs/v1.471.sf2')
 }
