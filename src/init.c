595080d3418be956cb4bf9e16eee467dc4a8304c
Jeroen Ooms <jeroenooms@gmail.com>
2024-02-17 14:06:09 +0100

    Set R log functions

--- src/init.c
+++ src/init.c
@@ -1,3 +1,4 @@
+#include <fluidsynth.h>
 #include <Rinternals.h>
 #include <stdlib.h>
 #include <R_ext/Rdynload.h>
@@ -12,7 +13,6 @@ extern SEXP C_fluidsynth_get_default(SEXP);
 extern SEXP C_midi_play(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP C_midi_read(SEXP, SEXP);
 
-
 static const R_CallMethodDef CallEntries[] = {
   {"C_fluidsynth_list_options", (DL_FUNC) &C_fluidsynth_list_options, 1},
   {"C_fluidsynth_get_default", (DL_FUNC) &C_fluidsynth_get_default, 1},
@@ -22,10 +22,18 @@ static const R_CallMethodDef CallEntries[] = {
   {NULL, NULL, 0}
 };
 
+static void logging_callback(int level, const char *message, void *data){
+  REprintf("[FluidSynth] %s\n", message);
+}
+
 void R_init_fluidsynth(DllInfo *dll){
 #ifdef HAS_LIBSDL2
    SDL_Init(SDL_INIT_AUDIO);
 #endif
   R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
   R_useDynamicSymbols(dll, FALSE);
+  fluid_set_log_function(FLUID_PANIC, logging_callback, NULL);
+  fluid_set_log_function(FLUID_ERR, logging_callback, NULL);
+  fluid_set_log_function(FLUID_WARN, logging_callback, NULL);
+  fluid_set_log_function(FLUID_INFO, logging_callback, NULL);
 }
